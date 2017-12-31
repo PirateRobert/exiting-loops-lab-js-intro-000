0fc3058614f1d3e9a5ac3692250835384bfd6a56
This is Rude <dbauman1@swarthmore.edu>
2017-12-31 19:28:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function breakOut(array,changeValue,stopValue){
-  for(let i = 0;i<array.length;i++)
+  for(let i = 0;i<array.length;i++){
+    if (array[i]===stopValue){
+      break
+    }
+    array[i] = changeValue
+  }
+  return array
 }
