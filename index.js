ae13360e56b5a04b96d58984a33521fb028efa02
This is Rude <dbauman1@swarthmore.edu>
2017-12-31 19:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,10 @@ function keepGoing(array,changeValue, skipValue){
 }
 
 function findBy(array, findFn){
- array.forEach
+ for(let i = 0;i<array.length;i++){
+   if findFn(array[i]){
+     return
+   }
+ }
+ return null
 }
